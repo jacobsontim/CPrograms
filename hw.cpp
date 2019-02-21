8e8e0e41d278342c91c17352887cf9880865df97
jacobsontim <tim.jacobson@nexenta.com>
2019-02-21 04:22:53 +0000

    cleanup

--- hw.cpp
+++ hw.cpp
@@ -6,17 +6,11 @@ int main()
 {
   cout<<"Hello & Goodbye"<<endl;
 
-<<<<<<< HEAD
 
   // this is my first comment
-<<<<<<< HEAD
   // this is my second comment
   // this is my third comment
   // this is my fourth comment
-=======
  
->>>>>>> parent of c87ae95... first comment
-=======
->>>>>>> parent of c23c9f0... first comment
 	return 0;
 }
